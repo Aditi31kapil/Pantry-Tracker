49f8d0df6e9accfc7f340b9f6827eaa0f41dccf0
Aditi31kapil <151728032+Aditi31kapil@users.noreply.github.com>
2024-08-05 21:27:29 +0530

    Update main.js

--- inventory_management/main.js
+++ inventory_management/main.js
@@ -1,7 +1,7 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI("AIzaSyB9BJ38XwV1GFkkZTsp9rfTEG7IZP5ww5Q");
+const genAI = new GoogleGenerativeAI("your API KEY");
 
 // ...
 async function run() {
